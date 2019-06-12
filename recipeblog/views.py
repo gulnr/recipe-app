5a1dba04c3fd90e8310897ec61a25d3b51799c4c
kayagu16 <kayagu16@itu.edu.tr>
2019-06-12 16:48:21 +0300

    class based views created

---
+++ recipeblog/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.views.generic import (TemplateView,ListView, DetailView, CreateView, UpdateView, DeleteView)
+from recipeblog.models import Post, Comment
+from recipeblog.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
+
+# function based views use decorators | class based views use mixins
+# Create your views here.
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    # sql query filter  base on condition
+    # most recent blog post comes at the top view
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'recipeblog/post_detail.html'
+
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'recipeblog/post_detail.html'
+
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = '/recipeblog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
